ecb468ef4a5c6e3abc3a1ff9569f46bb63322c45
shivam saini <shivamsainier98@gmail.com>
2022-07-23 17:01:45 +0530

    add hero section and buttons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Hero from "./Components/HeroSection/Hero";
+import "./App.css"
 
 function App() {
   return (
--- src/Components/HeroSection/Hero.jsx
+++ src/Components/HeroSection/Hero.jsx
@@ -11,8 +11,43 @@ const Hero = () => {
                     <div></div>
                     <span>the best fitness club in the town </span>
                 </div>
+                {/* Hero Heading */}
+                    <div className="hero-text">
+                        <div>
+                            <span className="stroke-text">Shape </span>
+                            <span>Your</span>
+                        </div>
+                        <div>
+                            <span>Ieadl Body</span>
+                        </div>
+                        <div>
+                            <span>
+                                In here we will help help you to shape and build your ideal body and live up your life to fullest
+                            </span>
+                        </div>
+                    </div>
+                {/* Figure Section  */}
+                <div className="figures">
+                    <div>
+                        <span>+140</span>
+                        <span>Expert Coaches</span>
+                    </div>
+                    <div>
+                        <span>+978</span>
+                        <span>members</span>
+                    </div>
+                    <div>
+                        <span>+50</span>
+                        <span>fitness programs</span>
+                    </div>
+                </div>
+                {/* Hero Buttons */}
+                <div className="hero-buttons">
+                    <button className="btn">Get Started</button>
+                    <button className="btn">Learn More</button>
+                </div>
+            
             </div>
-
             <div className='right-h'>right Side</div>
         </div>
       
